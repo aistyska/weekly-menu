a69ee9b8090c5d13c15340c7f550081625ee7d51
Aiste Laugayte <aiste.laugalyte@gmail.com>
2020-07-31 19:49:59 +0300

    Improved home page appearance

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,12 @@
 @section('title', 'Pagrindinis')
 
 @section('content')
-    <h2>Sveiki atvykę į Weekly Menu!</h2>
-    <p>Aš žinau daug receptų ir galiu sukurti savaitės meniu.</p>
-    <a class="btn btn-outline-dark" href="{{route('newRecipe')}}" role="button">Pridėti naują receptą</a>
-    <a class="btn btn-outline-dark" href="{{ route('genType') }}" role="button">Sukurti savaitės meniu</a>
+
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Sveiki atvykę į Weekly Menu!</h1>
+        <p class="lead">Aš žinau daug receptų ir galiu sukurti savaitės meniu.</p>
+        <hr class="my-4">
+        <a class="btn btn-outline-dark btn-lg mb-2 mb-sm-0" href="{{route('newRecipe')}}" role="button">Pridėti naują receptą</a>
+        <a class="btn btn-outline-dark btn-lg ml-sm-2 mb-2 mb-sm-0" href="{{ route('genType') }}" role="button">Sukurti savaitės meniu</a>
+    </div>
 @endsection
