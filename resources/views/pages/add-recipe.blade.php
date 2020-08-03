d3863895f39125a849f55aa507dc87b4146837e2
Aiste Laugayte <aiste.laugalyte@gmail.com>
2020-08-03 18:11:11 +0300

    Improved add new recipe, edit recipe form and recipe page layouts

--- resources/views/pages/add-recipe.blade.php
+++ resources/views/pages/add-recipe.blade.php
@@ -4,57 +4,49 @@
 
 @section('content')
 
-    <h2>Naujas receptas</h2>
+    <h2 class="mb-3">Naujas receptas</h2>
     <p>Užpildykite formą ir išsaugokite receptą</p>
     <form method="post" action="/save-recipe">
         @csrf
-        <div class="form-group row">
-            <label for="title" class="col-sm-2 col-form-label">Pavadinimas</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
-            </div>
-        </div>
+        <div class="form-group">
+            <label for="title">Pavadinimas</label>
+            <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
             @error('title')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        <div class="form-group row">
-            <label for="ingredients" class="col-sm-2 col-form-label">Ingredientai</label>
-            <div class="col-sm-10">
-                <textarea class="form-control" name="ingredients" id="ingredients" rows="5">{{old('ingredients')}}</textarea>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md">
+                <label for="ingredients">Ingredientai</label>
+                <textarea class="form-control" name="ingredients" id="ingredients" rows="7">{{old('ingredients')}}</textarea>
+                @error('ingredients')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-md">
+                <label for="instructions">Gaminimo eiga</label>
+                <textarea class="form-control" name="instructions" id="instructions" rows="7">{{old('instructions')}}</textarea>
+                @error('instructions')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
-            @error('ingredients')
-            <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        <div class="form-group row">
-            <label for="instructions" class="col-sm-2 col-form-label">Gaminimo eiga</label>
-            <div class="col-sm-10">
-                <textarea class="form-control" name="instructions" id="instructions" rows="5">{{old('instructions')}}</textarea>
-            </div>
-        </div>
-            @error('instructions')
-            <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        <div class="form-group row">
-            <label for="url" class="col-sm-2 col-form-label">Nuoroda į šaltinį</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" name="url" id="url" value="{{ old('url') }}">
-            </div>
-        </div>
+        <div class="form-group">
+            <label for="url">Nuoroda į šaltinį</label>
+            <input type="text" class="form-control" name="url" id="url" value="{{ old('url') }}">
             @error('url')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        <div class="form-group row">
-            <label for="comment" class="col-sm-2 col-form-label">Komentarai</label>
-            <div class="col-sm-10">
-                <textarea class="form-control" name="comment" id="comment" rows="3">{{old('comment')}}</textarea>
-            </div>
         </div>
+        <div class="form-group">
+            <label for="comment" >Komentarai</label>
+            <textarea class="form-control" name="comment" id="comment" rows="3">{{old('comment')}}</textarea>
             @error('comment')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-        <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#saveRecipeModal">Išsaugoti</button>
-        <a class="btn btn-outline-danger" href="/" role="button">Atšaukti</a>
+        </div>
+        <button type="button" class="btn btn-outline-success m-1" data-toggle="modal" data-target="#saveRecipeModal">Išsaugoti</button>
+        <a class="btn btn-outline-danger m-1" href="{{ route('allRecipes') }}" role="button">Atšaukti</a>
 
         <!-- Modal -->
         <div class="modal fade" id="saveRecipeModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
--- resources/views/pages/one-recipe.blade.php
+++ resources/views/pages/one-recipe.blade.php
@@ -11,20 +11,26 @@
     @endif
 
     <a class="btn btn-outline-dark" href="{{ route('allRecipes') }}">Visi receptai</a>
-    <h2>{{$recipe->title}}</h2>
-    <h4>Ingredientai</h4>
-    <p>{!! nl2br(e($recipe->ingredients)) !!}</p>
-    <h4>Gaminimo eiga</h4>
-    <p>{!! nl2br(e($recipe->instructions)) !!}</p>
+    <h2 class="my-3">{{$recipe->title}}</h2>
+    <div class="row">
+        <div class="col-md-5">
+            <h4><u>Ingredientai</u></h4>
+            <p>{!! nl2br(e($recipe->ingredients)) !!}</p>
+        </div>
+        <div class="col-md">
+            <h4><u>Gaminimo eiga</u></h4>
+            <p>{!! nl2br(e($recipe->instructions)) !!}</p>
+        </div>
+    </div>
     @isset($recipe->comment)
-    <h5>Komentarai</h5>
+    <h5><u>Komentarai</u></h5>
     <p>{{$recipe->comment}}</p>
     @endisset
     @isset($recipe->url)
-    <a href="{{$recipe->url}}" target="_blank" class="btn btn-outline-dark">Šaltinis</a>
+    <a href="{{$recipe->url}}" target="_blank" class="btn btn-outline-dark m-1">Šaltinis</a>
     @endisset
-    <a href="{{ route('editRecipe', ['recipe' => $recipe->id]) }}" class="btn btn-outline-dark">Redaguoti</a>
-    <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteRecipeModal">Ištrinti</button>
+    <a href="{{ route('editRecipe', ['recipe' => $recipe->id]) }}" class="btn btn-outline-dark m-1">Redaguoti</a>
+    <button type="button" class="btn btn-outline-danger m-1" data-toggle="modal" data-target="#deleteRecipeModal">Ištrinti</button>
 
     <!-- Modal -->
     <div class="modal fade" id="deleteRecipeModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
