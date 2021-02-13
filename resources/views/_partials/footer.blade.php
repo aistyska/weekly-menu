75bddd49397c3d311fe12af11378504b171da5c7
Aiste Laugayte <aiste.laugalyte@gmail.com>
2021-02-13 17:06:25 +0200

    Changed the year

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-3">
     <div class="container text-center">
-        <span class="text-muted">Aistė Laugaytė 2020</span>
+        <span class="text-muted">Aistė Laugaytė 2021</span>
     </div>
 </footer>
 
